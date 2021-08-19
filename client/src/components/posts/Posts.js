a614e14ca593464206d68df637d220dc70538808
bikash <vikashdash004@gmail.com>
2021-08-19 22:12:30 +0530

    added style

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Post from './post/Post'
+export default function Posts() {
+    return (
+        <div>
+            <h1>Posts</h1>
+            <Post/>
+        </div>
+    )
+}
