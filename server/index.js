2e6001e5f742a254856adc091c0076a2c37c4fc6
bikash <vikashdash004@gmail.com>
2021-08-19 20:28:39 +0530

    seted mongoose

--- server/index.js
+++ server/index.js
@@ -8,3 +8,16 @@ const app = express();
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://bikash:bikash123@cluster0.j6es8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.port | 5000;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>app.listen(PORT,()=>{
+    console.log(`Server is running on port:${PORT}`);
+}))
+.catch((error)=>{
+    console.log(error.message);
+})
+
+mongoose.set('useFindAndModify',false); 
