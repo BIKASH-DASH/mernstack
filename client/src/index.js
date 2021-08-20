85cd42bb6dd9bbac801021d6ff63fe21d22cb4fb
bikash <vikashdash004@gmail.com>
2021-08-20 08:16:09 +0530

    added reducers

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,10 @@ import reactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import {createStore,applyMiddleware,compose } from 'redux'
 import thunk from 'redux-thunk'
-import App from './App'
-
+import reducers from './reducers'
 const store = createStore(reducers,compose(applyMiddleware(thunk)))
 
+import App from './App'
 
 reactDOM.render(
 <Provider>
