3e89da48fb6b1e20ca3f06f41cc12567e368cca8
bikash <vikashdash004@gmail.com>
2021-08-20 07:50:56 +0530

    post data

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Post from './post/Post'
+import useStyles from './styles'
 export default function Posts() {
+    const classes = useStyles();
     return (
         <div>
             <h1>Posts</h1>
--- client/src/components/posts/post/Post.js
+++ client/src/components/posts/post/Post.js
@@ -1,6 +1,7 @@
 import React from 'react'
-
+import useStyles from './styles'
 export default function Post() {
+    const classes = useStyles();
     return (
         <div>
             <h1>post</h1>
